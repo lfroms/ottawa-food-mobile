d0db3bf12adeb9f44eeed98049e8579a208fbb1b
Lukas Romsicki <lukas@romsicki.com>
2020-04-12 14:36:49 -0400

    Extract to scrollable labeled section

--- OttawaFood/Sections/ForYou/Subsections/Trending/TrendingSection.swift
+++ OttawaFood/Sections/ForYou/Subsections/Trending/TrendingSection.swift
@@ -10,17 +10,11 @@ import SwiftUI
 
 struct TrendingSection: View {
     var body: some View {
-        LabeledSection(label: "Trending", labelInset: EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0)) {
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 14) {
-                    RestaurantCard(text: "Test", imageUrl: URL(string: "asdf")!, size: CGSize(width: 162, height: 131))
-                    RestaurantCard(text: "Test", imageUrl: URL(string: "asdf")!, size: CGSize(width: 162, height: 131))
-                    RestaurantCard(text: "Test", imageUrl: URL(string: "asdf")!, size: CGSize(width: 162, height: 131))
-                    RestaurantCard(text: "Test", imageUrl: URL(string: "asdf")!, size: CGSize(width: 162, height: 131))
-                }
-                .padding(20)
-            }
-            .padding(.top, -20)
+        ScrollableStackLabeledSection(label: "Trending") {
+            RestaurantCard(text: "Test", imageUrl: URL(string: "asdf")!, size: CGSize(width: 162, height: 131))
+            RestaurantCard(text: "Test", imageUrl: URL(string: "asdf")!, size: CGSize(width: 162, height: 131))
+            RestaurantCard(text: "Test", imageUrl: URL(string: "asdf")!, size: CGSize(width: 162, height: 131))
+            RestaurantCard(text: "Test", imageUrl: URL(string: "asdf")!, size: CGSize(width: 162, height: 131))
         }
     }
 }
---
+++ OttawaFood/Views/LabeledSection/Scrollable/ScrollableStackLabeledSection.swift
@@ -0,0 +1,39 @@
+//
+//  ScrollableStackLabeledSection.swift
+//  OttawaFood
+//
+//  Created by Lukas Romsicki on 2020-04-12.
+//  Copyright © 2020 Lukas Romsicki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ScrollableStackLabeledSection<Content: View>: View {
+    private let label: String
+    private let content: () -> Content
+
+    @inlinable init(label: String, @ViewBuilder content: @escaping () -> Content) {
+        self.label = label
+        self.content = content
+    }
+
+    var body: some View {
+        LabeledSection(label: label, labelInset: EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0)) {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 14) {
+                    self.content()
+                }
+                .padding(20)
+            }
+            .padding(.top, -20)
+        }
+    }
+}
+
+struct ScrollableLabeledSection_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollableStackLabeledSection(label: "Test") {
+            Text("Test")
+        }
+    }
+}
